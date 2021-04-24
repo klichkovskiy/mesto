5ac21056774420684b347da695c5fb6a03f328dd
Evgeniy Klichkovkiy <klichkouski@gmail.com>
2021-04-25 02:14:37 +0300

    исправить код js, передаелать popup

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,16 @@
 let editButton = document.querySelector('.profile__edit-button');
-let overlayForm = document.querySelector('.overlay');
+let overlayForm = document.querySelector('.overlay__form');
+let overlayOpen = document.querySelector('.overlay');
 let overlayClose = document.querySelector('.overlay__close');
-let nameInput = document.querySelector('.overlay__name');
-let captionInput = document.querySelector('.overlay__caption');
+let nameInput = document.querySelector('.overlay__item_name');
+let captionInput = document.querySelector('.overlay__item_caption');
 let profileName = document.querySelector('.profile__name');
 let profileCaption = document.querySelector('.profile__caption');
 
 function overlayClick(){
-  overlayForm.classList.toggle('overlay__opened');
+  nameInput.value = profileName.textContent;
+  captionInput.value = profileCaption.textContent;
+  overlayOpen.classList.toggle('overlay_opened');
 };
 editButton.addEventListener('click', overlayClick);
 overlayClose.addEventListener('click', overlayClick);
