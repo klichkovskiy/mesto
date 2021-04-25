aa8c9d3ae2c17fb0a2b7d0ae5a7fa53c22d1a09a
Evgeniy Klichkovkiy <klichkouski@gmail.com>
2021-04-25 14:28:05 +0300

    переименовать наименование классов полей ввода

--- scripts/index.js
+++ scripts/index.js
@@ -2,8 +2,8 @@ let editButton = document.querySelector('.profile__edit-button');
 let overlayForm = document.querySelector('.overlay__form');
 let overlayOpen = document.querySelector('.overlay');
 let overlayClose = document.querySelector('.overlay__close');
-let nameInput = document.querySelector('.overlay__item_name');
-let captionInput = document.querySelector('.overlay__item_caption');
+let nameInput = document.querySelector('.overlay__item_profile_name');
+let captionInput = document.querySelector('.overlay__item_profile_caption');
 let profileName = document.querySelector('.profile__name');
 let profileCaption = document.querySelector('.profile__caption');
 
