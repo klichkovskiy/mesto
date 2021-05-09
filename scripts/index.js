bafefd06f1c1247a7abb6a264878f2fa39cbb7fa
Evgeniy Klichkovkiy <klichkouski@gmail.com>
2021-05-09 23:37:11 +0300

    Готовая работа

--- scripts/index.js
+++ scripts/index.js
@@ -55,32 +55,36 @@ const initialCards = [
 ];
 
 
+//функция открытия и закрытия popupов
+function openAndClosePopup (popupNameConst) {
+  popupNameConst.classList.toggle('overlay_opened');
+};
 
 //функция открытия формы изменения имени и рода деятельности
 function openOverlayProfileClick() {
+  openAndClosePopup (overlayProfileOpen);
   nameProfileInput.value = profileName.textContent;
   captionProfileInput.value = profileCaption.textContent;
-  overlayProfileOpen.classList.toggle('overlay_opened');
 };
 editButton.addEventListener('click', openOverlayProfileClick);
 
 //функция открытия формы создания карточек
 function openOverlayCardClick() {
+  openAndClosePopup (overlayCardOpen);
   nameCardInput.value = '';
   captionCardInput.value = '';
-  overlayCardOpen.classList.toggle('overlay_opened');
 };
 addButton.addEventListener('click', openOverlayCardClick);
 
 //функция закрытия без сохранения формы изменения имени и рода деятельности
 function closeOverlayProfileClick() {
-  overlayProfileOpen.classList.toggle('overlay_opened');
+  openAndClosePopup (overlayProfileOpen);
 };
 overlayProfileClose.addEventListener('click', closeOverlayProfileClick);
 
 //функция закрытия без сохранения формы создания карточек
 function closeOverlayCardClick() {
-  overlayCardOpen.classList.toggle('overlay_opened');
+  openAndClosePopup (overlayCardOpen);
 };
 overlayCardClose.addEventListener('click', closeOverlayCardClick);
 
@@ -93,37 +97,41 @@ function saveFormSubmitProfileHandler(evt) {
 }
 overlayProfileForm.addEventListener('submit', saveFormSubmitProfileHandler);
 
-
 //функция закрытия просмотра фотографии
 const overlayImage = document.querySelector('#overlay-image');
 const overlayImageClose = overlayImage.querySelector('.overlay__close');
 overlayImageClose.addEventListener('click', function () {
-  console.log('exit')
-  overlayImage.classList.toggle('overlay_opened');
+  openAndClosePopup (overlayImage);
 });
 
-
-//функция поставки лайка
+//функция обработки лайка
 function putLike(evt) {
   evt.target.classList.toggle('card__like_aktive');
 };
 
+// функция создания элемента из данных для карточки
+function createCard(cardName, cardLink) {
+  const cardNew = card.cloneNode(true);
+  cardNew.querySelector('.card__name').textContent = cardName;
+  cardNew.querySelector('.card__image').src = cardLink;
+  cardNew.querySelector('.card__image').alt = cardName;
+  return cardNew
+};
+
+// функция добавления элемента в массив
+function renderCard(cardNew) {
+  cardsBlock.prepend(cardNew);
+}
 
 //функция создания новых карточек и возможность ставить лайк, удалить карточку, открыть фото
 function formSubmitCardHandler(evt) {
-  const cardNew = card.cloneNode(true);
-  const cardName = cardNew.querySelector('.card__name');
-  const cardImage = cardNew.querySelector('.card__image');
+  const cardNew = createCard(nameCardInput.value, captionCardInput.value);
   evt.preventDefault();
-  cardName.textContent = nameCardInput.value;
-  cardImage.src = captionCardInput.value;
-  cardsBlock.prepend(cardNew);
+  renderCard(cardNew);
   closeOverlayCardClick();
-
 
   //поставка лайка
   cardNew.querySelector('.card__like').addEventListener('click', putLike);
-
 
   //установка слушателя и  удаление карточки с фотографией
   const cardDelete = cardNew.querySelector('#card-delete');
@@ -131,14 +139,14 @@ function formSubmitCardHandler(evt) {
     const listItem = cardDelete.closest('.card');
     listItem.remove();
   });
-
-
+  const cardImage = cardNew.querySelector('.card__image');
+  const cardName = cardNew.querySelector('.card__name');
   //установка слушателя и  открытие просмотра фотографии
   cardImage.addEventListener('click', function (evt) {
     const overlayImage = document.querySelector('#overlay-image');
     const overlayImageName = overlayImage.querySelector('.overlay__name');
     const overlayImageItem = overlayImage.querySelector('.overlay__image');
-    overlayImage.classList.toggle('overlay_opened');
+    openAndClosePopup (overlayImage);
     overlayImageName.textContent = cardName.textContent;
     overlayImageItem.src = cardImage.src;
     overlayImageItem.alt = cardName.textContent;
@@ -147,22 +155,14 @@ function formSubmitCardHandler(evt) {
 overlayCardForm.addEventListener('submit', formSubmitCardHandler);
 
 
-
 //функция создания стартовых карточек и возможность ставить лайк, удалить карточку, открыть фото
 function initialStartCards() {
   for (let i = 0; i <= initialCards.length; i++) {
-    const cardNew = card.cloneNode(true);
-    const cardName = cardNew.querySelector('.card__name');
-    const cardImage = cardNew.querySelector('.card__image');
-    cardName.textContent = initialCards[i].name;
-    cardImage.src = initialCards[i].link;
-    cardImage.alt = initialCards[i].name;
-    cardsBlock.prepend(cardNew);
-
+    const cardNew = createCard(initialCards[i].name, initialCards[i].link);
+    renderCard(cardNew);
 
     //поставка лайка
     cardNew.querySelector('.card__like').addEventListener('click', putLike);
-
 
     //установка слушателя и  удаление карточки с фотографией
     const cardDelete = cardNew.querySelector('#card-delete');
@@ -170,14 +170,14 @@ function initialStartCards() {
       const listItem = cardDelete.closest('.card');
       listItem.remove();
     });
-
-
+    const cardImage = cardNew.querySelector('.card__image');
+    const cardName = cardNew.querySelector('.card__name');
     //установка слушателя и открытие просмотра фотографии
     cardImage.addEventListener('click', function (evt) {
       const overlayImage = document.querySelector('#overlay-image');
       const overlayImageName = overlayImage.querySelector('.overlay__name');
       const overlayImageItem = overlayImage.querySelector('.overlay__image');
-      overlayImage.classList.toggle('overlay_opened');
+      openAndClosePopup (overlayImage);
       overlayImageName.textContent = cardName.textContent;
       overlayImageItem.src = cardImage.src;
       overlayImageItem.alt = cardName.textContent;
