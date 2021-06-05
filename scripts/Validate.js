3953932c4a5f8a9231fe9c0408c2beba9be026c2
Evgeniy Klichkovkiy <klichkouski@gmail.com>
2021-06-05 09:17:19 +0300

    файлы классов с заглавной

---
+++ scripts/Validate.js
@@ -0,0 +1,71 @@
+export default class FormValidator {
+  constructor(config, formElement) {
+    this._config = config;
+    this._formElement = formElement;
+
+    this._inputList = Array.from(this._formElement.querySelectorAll(this._config.inputSelector));
+    this._buttonElement = this._formElement.querySelector(this._config.submitButtonSelector);
+  }
+
+  _setEventListeners() {
+    this._inputList.forEach((inputElement) => {
+      inputElement.addEventListener('input', () => {
+        this._checkInputValidity(inputElement);
+        this.toggleButtonState();
+      })
+    })
+    this.toggleButtonState();
+  }
+
+  _checkInputValidity(inputElement) {
+    if (inputElement.validity.valid) {
+      this._hideInputError(inputElement);
+    } else {
+      this._showInputError(inputElement);
+    };
+  }
+
+  _hideInputError(inputElement) {
+    const errorElement = this._formElement.querySelector(`.${inputElement.id}-error`);
+    inputElement.classList.remove(this._config.inputErrorClass);
+    errorElement.classList.remove(this._config.errorClass);
+    errorElement.textContent = '';
+  }
+
+  _showInputError(inputElement) {
+    const errorElement = this._formElement.querySelector(`.${inputElement.id}-error`);
+    inputElement.classList.add(this._config.inputErrorClass);
+    errorElement.classList.add(this._config.errorClass);
+    errorElement.textContent = inputElement.validationMessage;
+  }
+
+  _hasInvalidInput() {
+    return this._inputList.some(inputElement => !inputElement.validity.valid)
+  }
+
+  resetValidationform() {
+    this._inputList.forEach((inputElement) => {
+      this._hideInputError(inputElement);
+      this.toggleButtonState();
+      
+  })
+  this._config.formSelector.reset();
+}
+
+  toggleButtonState() {
+    if (this._hasInvalidInput()) {
+      this._buttonElement.classList.add(this._config.inactiveButtonClass);
+      this._buttonElement.disabled = true;
+    } else {
+      this._buttonElement.classList.remove(this._config.inactiveButtonClass);
+      this._buttonElement.disabled = false;
+    }
+  }
+
+  enableValidation() {
+    this._formElement.addEventListener('submit', (evt) => {
+      evt.preventDefault();
+    });
+    this._setEventListeners();
+  }
+}
